2f17c55040bce006bd55e28b758ab19a09de0898
Oleksandr_Tokarenko <oleksandr.tokarenko@refinitiv.com>
2020-02-05 17:43:49 +0200

    Starter pack to run the project;

---
+++ src/main/java/com/alex/controllers/HelloJenkinsController.java
@@ -0,0 +1,13 @@
+package com.alex.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloJenkinsController {
+
+    @GetMapping("/")
+    public String heelo() {
+        return "<h1>Hello Jenkins</h1>";
+    }
+}
